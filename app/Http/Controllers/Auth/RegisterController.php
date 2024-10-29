1bba71c6dd5429c9b09b1a9dbaf43acb0fdb06d2
Amr Achraf <amrachraf6690@icloud.com>
2024-10-29 11:11:19 +0300

    FIX and FIx

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -17,45 +17,36 @@ class RegisterController extends Controller
 
     public function store(RegisterRequest $request)
     {
-
         $data = $request->validated();
         $data['phone'] = '+' . $data['countryCode'] . $data['phone'];
-
         unset($data['countryCode']);
 
+        if ($data['type'] === 'user') {
+            $user = User::create([
+                'first_name' => $data['first_name'],
+                'last_name' => $data['last_name'],
+                'email' => $data['email'],
+                'phone' => $data['phone'],
+                'password' => $data['password'],
+            ]);
 
-        if($data['type'] === 'user'){
-            $user = User::create(
-                [
-                    'first_name' => $data['first_name'],
-                    'last_name' => $data['last_name'],
-                    'email' => $data['email'],
-                    'phone' => $data['phone'],
-                    'password' => $data['password'],
-                    'type' => $data['type'],
-                ]
-            );
-
-        auth()->login($user);
-
-        return redirect()->route('home');
+            auth()->login($user);
+            return redirect()->route('home');
         }
 
-        $user = Provider::create(
-            [
-                'name' => $data['first_name'] . ' ' . $data['last_name'],
-                'tag' => $data['tag'],
-                'type' => $data['type'] == 'Individual providers' ? 'individual' : 'company',
-                'phone' => $data['phone'],
-                'email' => $data['email'],
-                'password' => $data['password'],
-                'website' => $data['website'],
-                'company_id' => $data['combined_company_code'],
-            ]
-            );
+        $provider = Provider::create([
+            'name' => $data['first_name'] . ' ' . $data['last_name'],
+            'tag' => $data['tag'],
+            'type' => $data['type'] == 'Individual providers' ? 'individual' : 'company',
+            'phone' => $data['phone'],
+            'email' => $data['email'],
+            'password' => $data['password'],
+            'website' => $data['website'],
+            'company_id' => $data['combined_company_code'],
+        ]);
 
-        auth()->login($user);
-        
+        auth()->guard('provider')->login($provider);
         return redirect()->route('provider-panel.home');
     }
+
 }
